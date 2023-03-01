e41eee94c73e4528ddd5fa3e6aa4e1d3eb4c661c
chrisjonesonline <85334644+chrisjonesonline@users.noreply.github.com>
2023-02-28 17:31:47 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>CSS Cube</title>
+<link rel="stylesheet" href="assets/css/style.css"/>
+</head>
+<body>
+
+ <div class="cube">
+    <div class="face top">HTML</div>
+    <div class="face bottom">CSS</div>
+    <div class="face left">JS</div>
+    <div class="face right">jQuery</div>
+    <div class="face front">PHP</div>
+    <div class="face back">MySQL</div>
+  </div>
+
+</body>
+</html>
